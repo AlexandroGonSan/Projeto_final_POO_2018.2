f4038d37f5e37b4a64fd284a3ece1c95bf180b92
Luigi Muller <sluigimuller@gmail.com>
2018-09-22 20:34:03 -0400

    Codigo do programa
    Criação do projeto do programa no software NetBeans para testar se é possível trabalhar colaborativamente

---
+++ Código/Sistema de Estoque/src/sistema/de/estoque/Fornecedor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.de.estoque;
+
+/**
+ *
+ * @author Luigi Muller
+ */
+public class Fornecedor {
+    String nome;
+    String CNPJ;
+    String produtoFornecido;
+    
+    public void cadastrarFornecedor(){
+        
+    }
+    public void excluirFornecedor(){
+        
+    }
+    public void buscarFornecedor(){
+        
+    }
+    public void alterarFornecedor(){
+        
+    }
+}
