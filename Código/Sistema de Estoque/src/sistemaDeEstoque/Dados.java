f3509b1f6aa57552965b3ffd68de6ec40f73ac21
Luigi Muller <sluigimuller@gmail.com>
2018-11-18 21:20:01 -0400

    Javadoc da classe Dados

--- Código/Sistema de Estoque/src/sistemaDeEstoque/Dados.java
+++ Código/Sistema de Estoque/src/sistemaDeEstoque/Dados.java
@@ -1,6 +1,3 @@
-/*
-    *@author LuluTeam
-*/
 package sistemaDeEstoque;
 
 import java.io.FileInputStream;
@@ -12,35 +9,44 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
- * Classe usada para controlar os dados da interface.
- *  * @author LuluTeam
+ * Classe usada para controlar os dados da interface com o usuário.
+ * @author LuluTeam
  */
 public class Dados implements Serializable {
 
     /**
-     *IDdado atributo para controlar o ID do produto
+     * IDdado é o atributo para controlar o ID dos produtos.
      */
     public static int IDdado = 1;
 
     /**
-     *IDForn atributo para controlar o ID do fornecedor
+     * IDForn é o atributo para controlar o ID dos fornecedores.
      */
     public static int IDForn = 1;
     
+    /**
+     * IDRela é o atributo para controlar o ID do relatórios.
+     */
     public static int IDRela = 1;
 
     /**
-     *Array dados é onde os dados dos produtos são armazenados
+     * ArrayList dados é onde os dados dos produtos são armazenados.
      */
     public static ArrayList<Produto> dados = new ArrayList<Produto>();
     /**
-     *Array Forn é onde os dados dos fornecedores são armazenados
+     * ArrayList Forn é onde os dados dos fornecedores são armazenados.
      */
     public static ArrayList<Fornecedor> Forn = new ArrayList<Fornecedor>();
     
-    //
+    /**
+     * ArrayList relatorios é onde os dados dos relatorios são armazenados.
+     */
     public static ArrayList<Relatorio> relatorios  = new ArrayList<Relatorio>();
     
+    /**
+     * O método busca pelo arquivo que contém os dados dos produtos salvos anteriormente 
+     * e carrega para o programa, caso o arquivo não exista, ele exibe um aviso de erro no terminal.
+     */
     public static void deserializarProdutos(){
         try
         {
@@ -84,6 +90,9 @@ public class Dados implements Serializable {
         }
     }
     
+    /**
+     * Esse método serializa os dados dos fornecedores em um arquivo e em caso de erro, ele emite no terminal o erro.
+     */
     public static void serializarFornecedor(){
         try {
             FileOutputStream arquivoGrav = new FileOutputStream("TesteFornecedor.txt");
@@ -99,6 +108,10 @@ public class Dados implements Serializable {
         }
     }
     
+    /**
+     * O método busca pelo arquivo que contém os dados dos relatorios salvos anteriormente 
+     * e carrega para o programa, caso o arquivo não exista, ele exibe um aviso de erro no terminal.
+     */
     public static void deserializarRelatorio(){
         try {
             FileInputStream arquivoLeitura = new FileInputStream ("Relatorios.txt");
@@ -113,6 +126,9 @@ public class Dados implements Serializable {
         }
     }
     
+    /**
+     * Esse método serializa os relatórios em um arquivo e em caso de erro, ele emite no terminal o erro.
+     */
     public static void serializarRelatorio(){
         try {
             FileOutputStream arquivoGrav = new FileOutputStream("Relatorios.txt"); 
@@ -124,7 +140,7 @@ public class Dados implements Serializable {
             arquivoGrav.close();
         } 
         catch(IOException e) {
-            System.out.println("ERROR! não foi possível abrir o arquivo! " + e.getMessage());
+            System.out.println("ERROR! não foi possível serializar os relatórios! " + e.getMessage());
         }
     }
 }
